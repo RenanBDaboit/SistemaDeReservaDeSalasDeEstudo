3fa3b7aa5271e23020dc3098eeffc3764b6686db
joao_ullmann <joao_ullmann@estudante.sesisenai.org.br>
2026-05-15 16:32:26 -0300

    joao

---
+++ src/entity/Reserva.java
@@ -0,0 +1,69 @@
+package entity;
+
+public class Reserva {
+
+    private int id;
+    private Usuario usuario;
+    private Sala sala;
+    private String data;
+    private String horario;
+    private String status;
+
+    public Reserva(int id, Usuario usuario, String data, String horario, String status, Sala sala) {
+        this.id = id;
+        this.usuario = usuario;
+        this.data = data;
+        this.horario = horario;
+        this.status = status;
+        this.sala = sala;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
