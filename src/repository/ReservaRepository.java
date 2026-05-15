44731a7a5c3ae6e3703279a323a30335993b72fd
renan_daboit <renan_daboit@estudante.sesisenai.org.br>
2026-05-15 18:31:12 -0300

    Atualização das classes repository

--- src/repository/ReservaRepository.java
+++ src/repository/ReservaRepository.java
@@ -6,9 +6,21 @@ import java.util.HashMap;
 
 public class ReservaRepository {
 
-    private final HashMap<Integer, Reserva> resservas = new HashMap<>();
+    private final HashMap<Integer, Reserva> reservas = new HashMap<>();
 
-    public HashMap<Integer, Reserva> getResservas() {
-        return resservas;
+    public void salvar(Reserva reserva){
+        reservas.put(reserva.getId(), reserva);
+    }
+
+    public Reserva buscar(int id){
+        return reservas.get(id);
+    }
+
+    public HashMap<Integer, Reserva> listar() {
+        return reservas;
+    }
+
+    public void remover(int id){
+        reservas.remove(id);
     }
 }
